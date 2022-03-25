2e397c8b4d0c60853e6041e79aa574991586eee9
AnastasiaEfremova <anastasia.efremova@stoloto.ru>
2022-03-25 13:30:58 +0300

    New Tasks

---
+++ src/com/company/Level_13/DeleteLastStringAndPutItAtTheBegining/Solution.java
@@ -0,0 +1,33 @@
+package com.company.Level_13.DeleteLastStringAndPutItAtTheBegining;
+
+/*
+Удали последнюю строку и вставь её в начало
+
+Создай список строк.
+Добавь в него 5 строк с клавиатуры.
+Выполни 13 раз: удалить последнюю строку и вставить её в начало.
+Используя цикл, выведи содержимое результирующего списка на экран, каждое значение с новой строки.
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            list.add(reader.readLine());
+        }
+
+        for (int i = 0; i < 13; i++) {
+            list.add(0, list.remove(4));
+        }
+
+        for (String x: list) {
+            System.out.println(x);
+        }
+    }
+}
