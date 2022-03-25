079a3c3e407fed1801b8f51777a7621b87473c87
AnastasiaEfremova <anastasia.efremova@stoloto.ru>
2022-03-25 12:35:03 +0300

    Initial commit

---
+++ src/com/company/Level_13/EasyList/Solution.java
@@ -0,0 +1,28 @@
+package com.company.Level_13.EasyList;
+
+import java.util.ArrayList;
+
+/* 
+5 различных строк в списке
+*/
+
+public class Solution {
+    
+    public static ArrayList<String> list;
+    
+    public static void main(String[] args) {
+        list = new ArrayList<>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        list.add("four");
+        list.add("five");
+
+        System.out.println(list.size());
+        System.out.println("---------------------------");
+
+        for (String x: list) {
+            System.out.println(x);
+        }
+    }
+}
---
+++ src/com/company/Level_13/TheBiggestNumber/Solution.java
@@ -0,0 +1,26 @@
+package com.company.Level_13.TheBiggestNumber;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/* 
+Самое большое число
+*/
+
+public class Solution {
+
+    private static ArrayList<Integer> integers = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 5; i++) {
+            integers.add(Integer.parseInt(reader.readLine()));
+        }
+
+        int theBiggestNumber = Collections.max(integers);
+        System.out.println(theBiggestNumber);
+    }
+}
