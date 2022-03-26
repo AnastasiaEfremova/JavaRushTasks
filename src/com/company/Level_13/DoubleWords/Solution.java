98167bb45cde03e6002c8e0af00f9726a15076af
AnastasiaEfremova <anastasia.efremova@stoloto.ru>
2022-03-26 09:06:32 +0300

    New Tasks

---
+++ src/com/company/Level_13/DoubleWords/Solution.java
@@ -0,0 +1,39 @@
+package com.company.Level_13.DoubleWords;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Удвой слова
+
+Введи с клавиатуры 5 слов в список строк.
+Метод doubleValues должен удваивать слова по принципу
+"альфа", "бета", "гамма" -> "альфа", "альфа", "бета", "бета", "гамма", "гамма".
+Выведи результат на экран, каждое значение с новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(reader.readLine());
+        }
+
+        ArrayList<String> result = doubleValues(list);
+
+        for (String x: result) {
+            System.out.println(x);
+        }
+    }
+
+    public static ArrayList<String> doubleValues(ArrayList<String> list) {
+        ArrayList<String> result = new ArrayList<>();
+        for (String x: list) {
+            result.add(x);
+            result.add(x);
+        }
+        return result;
+    }
+}
